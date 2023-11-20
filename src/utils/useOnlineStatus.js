d984d7aa0a8f8d00b108434502b587ab0121ff6e
Sravani020894 <sravani.mgit@gmail.com>
2023-11-20 14:42:37 -0500

    added onlinestatus

--- src/utils/useOnlineStatus.js
+++ src/utils/useOnlineStatus.js
@@ -1,20 +1,49 @@
+// import { useEffect, useState } from "react";
+
+// const useOnlineStatus = () => {
+//   const [onlineStatus, setOnlineStatus] = useState(true);
+
+//   useEffect(() => {
+//     window.addEventListener("offline", () => {
+//       setOnlineStatus(false);
+//     });
+
+//     window.addEventListener("online", () => {
+//       setOnlineStatus(true);
+//     });
+//   }, []);
+
+//   // boolean value
+//   return onlineStatus;
+// };
+
+// export default useOnlineStatus;
+
 import { useEffect, useState } from "react";
 
 const useOnlineStatus = () => {
-  const [onlineStatus, setOnlineStatus] = useState(true);
-
-  useEffect(() => {
-    window.addEventListener("offline", () => {
+  let [onlineStatus,setOnlineStatus] = useState(true);
+  console.log("rendering page at first");
+    window.addEventListener("offline",()=>{
+      console.log("rendering page after offline");
       setOnlineStatus(false);
     });
-
-    window.addEventListener("online", () => {
+    window.addEventListener("online",()=>{
+      console.log("rendering page after online");
       setOnlineStatus(true);
-    });
-  }, []);
-
-  // boolean value
+  });
   return onlineStatus;
-};
+}
 
 export default useOnlineStatus;
+
+
+
+
+
+
+
+
+
+
+
